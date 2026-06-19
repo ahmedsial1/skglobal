7b65b8431f4630006a764fe9304dd5e0559c5fa7
Ahmed Sial <ahmedsial9331@gmail.com>
2026-06-19 15:17:24 +0800

    fix: increase global main wrapper padding-top to prevent header overlaps

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
         <LanguageProvider>
           <Navbar />
           {/* Main layout container with top padding to prevent fixed navbar overlap */}
-          <main className="flex-grow pt-20">
+          <main className="flex-grow pt-32 md:pt-40">
             {children}
           </main>
           <Footer />
